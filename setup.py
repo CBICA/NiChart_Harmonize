70739d0927a7fefc38a85ffb6b39035152d268d9
George Aidinis <gaidinis@live.com>
2024-05-24 17:40:17 -0400

    Minor fix to long description in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from setuptools import setup
+from pathlib import Path
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(name='NiChartHarmonize',
         version='2.2.0',
         description='Harmonization tools for multi-center neuroimaging studies.',
-        long_description=readme(),
+	long_description=long_description,
+	long_description_content_type='text/markdown',
         url='https://github.com/rpomponiohttps://github.com/gurayerus/NiChartHarmonize',
         author='Guray Erus',
         author_email='guray.erus@pennmedicine.upenn.edu',
